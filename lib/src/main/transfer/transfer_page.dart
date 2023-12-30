cd7952c0ce035f902b4c24b59371fe6671f93d59
M Atheer <55885230+masreplay@users.noreply.github.com>
2023-12-30 06:16:31 +0300

    Refactor TransferPage and add new widget classes

--- lib/src/main/transfer/transfer_page.dart
+++ lib/src/main/transfer/transfer_page.dart
@@ -7,40 +7,110 @@ class TransferPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    return Scaffold(
+      body: Responsive.when(
+        context: context,
+        compact: _TransferPageCompact.new,
+        medium: _TransferPageOrElse.new,
+        expanded: _TransferPageOrElse.new,
+      ),
+    );
+  }
+}
+
+class _TransferPageCompact extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
     final l10n = context.l10n;
     final theme = Theme.of(context);
     final colorScheme = theme.colorScheme;
     final textTheme = theme.textTheme;
 
-    return Scaffold(
-      appBar: AppBar(title: Text(l10n.transfer)),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: ColumnPadded(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            const AspectRatio(
-              aspectRatio: 16 / 9,
-              child: Placeholder(),
+    return Padding(
+      padding: const EdgeInsets.all(Insets.medium),
+      child: ColumnPadded(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        spacing: Insets.medium,
+        children: [
+          const AspectRatio(
+            aspectRatio: 16 / 9,
+            child: Placeholder(),
+          ),
+          Text(
+            l10n.transferServiceTitle,
+            style: textTheme.titleLarge?.copyWith(
+              color: colorScheme.primary,
             ),
-            Text(
-              l10n.transferServiceTitle,
-              style: textTheme.titleLarge?.copyWith(
-                color: colorScheme.primary,
-              ),
+          ),
+          Text(
+            l10n.transferServiceDescription,
+            style: textTheme.titleSmall?.copyWith(
+              color: colorScheme.onSurface,
             ),
-            Text(
-              l10n.transferServiceDescription,
-              style: textTheme.titleSmall?.copyWith(
-                color: colorScheme.onSurface,
-              ),
-            ),
-            FilledButton(
-              onPressed: () {},
-              child: Text(l10n.moneyTransfer),
-            ),
-          ],
+          ),
+          FilledButton.icon(
+            onPressed: () {},
+            icon: const Icon(DefaultIcons.transfer),
+            label: Text(l10n.moneyTransfer),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _TransferPageOrElse extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final l10n = context.l10n;
+    final theme = Theme.of(context);
+    final colorScheme = theme.colorScheme;
+    final textTheme = theme.textTheme;
+
+    const pane1 = AspectRatio(
+      aspectRatio: 9 / 16,
+      child: Placeholder(),
+    );
+
+    final pane2 = ColumnPadded(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      spacing: Insets.medium,
+      children: [
+        Text(
+          l10n.transferServiceTitle,
+          style: textTheme.titleLarge?.copyWith(
+            color: colorScheme.primary,
+          ),
         ),
+        Text(
+          l10n.transferServiceDescription,
+          style: textTheme.titleSmall?.copyWith(
+            color: colorScheme.onSurface,
+          ),
+        ),
+        FilledButton.icon(
+          onPressed: () {},
+          icon: const Icon(DefaultIcons.transfer),
+          label: Text(l10n.moneyTransfer),
+        ),
+      ],
+    );
+
+    const spacing = Insets.large;
+    return Padding(
+      padding: const EdgeInsets.all(spacing),
+      child: RowPadded(
+        spacing: spacing,
+        children: [
+          const Expanded(
+            flex: 2,
+            child: pane1,
+          ),
+          Expanded(
+            flex: 3,
+            child: pane2,
+          ),
+        ],
       ),
     );
   }
